c2cb4c996f802ebb9610eff6b7eae7cd5dfabfc3
kijong jo <cozacoza12@naver.com>
2020-05-05 16:26:46 +0900

    모듈: fileresizing 모듈
    파일 용량을 판단해 기준치를 넘을시 리사이징하는 모듈

---
+++ 모듈/fileresizing.js
@@ -0,0 +1,147 @@
+ //모듈: 파일 업로드시 프론트 단에서 용량을 줄인다. 줄이고싶은 용량크기를 정해 줄일 수 있다.
+
+
+ // 이미지 파일 blob 파일로 변환
+ function imgToBlob(img, file, form_Data) {
+     // canvas에 이미지 객체를 리사이징해서 담는 과정
+     var canvas = document.createElement("canvas");
+     var canvasContext = canvas.getContext("2d");
+
+     /* exp */
+     // 최대폭을 400으로 정했을 때
+     // 최대 폭을 넘어가는 경우 canvas 크기를 변경해준다.
+     // size기준으로 용량을 줄이게 되면 모양이 이상해 질수 있다.
+     // 크기 기준으로 줄이자.
+     // 크기기준으로 줄이기=> 루트(실제파일크기/축소하고싶은 용량 크기) =>나온 값 R=> (Width/R)
+     // *(Height/R) =>MaxSize
+     /* exp 끝 */
+
+     var base_size = 1024000; // 1MB (썸네일 작업을 할지 말지 기준이 되는size)
+     var comp_size = 102400; // 100kb (썸네일 작업 결과물 사이즈)
+
+     // 이미지 크기
+     var width = img.width;
+     var height = img.height;
+     var size = file.size;
+
+     console.log("리사이징 시작");
+
+     // size가 더 크면 blob 파일 전송
+     if (size > base_size) {
+         console.log("파일용량 기준치 초과 > blob객체화");
+         // 지정한 사이즈에 맞게 비율 조정하기
+         var ratio = Math.ceil(Math.sqrt((size / comp_size), 2));
+         // 높이와 넓이 비율에 따라 변환
+         width = img.width / ratio,
+             height = img.height / ratio;
+
+         // canvas 옵션에 줌
+         canvas.width = width;
+         canvas.height = height;
+         canvas.getContext('2d').drawImage(img, 0, 0, width, height);
+
+         // canvas의 dataurl를 blob(file)화 하는 과정
+         var dataURI = canvas.toDataURL("image/jpeg"); // png=>jpg 등으로 변환 가능
+
+         // !!!!!byteType과 mimeType으로 변환하기 . 개념이 조금 복잡해서 공부가 필요함
+         var byteString = atob(dataURI.split(',')[1]);
+         var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+         // 일반 고정 길이 이진 데이터 버퍼를 나타내는 데 사용되는 데이터형 바이너리 데이터 조작하기 위함
+         var ab = new ArrayBuffer(byteString.length);
+         var ia = new Uint8Array(ab);
+         for (var i = 0; i < byteString.length; i++) {
+             ia[i] = byteString.charCodeAt(i);
+         }
+         var tmpThumbFile = new Blob([ab], {
+             type: mimeString
+         });
+         // bytetype으로 변환하기 끝
+
+         var thumbFile = tmpThumbFile; // 여기서 이미지 객체 img를 활용하여 썸네일 처리를 할 수 있음
+         console.log("리사이징전 크기: " + thumbFile.size); // 크기가 1자리 이상 줄어듬
+         console.log("리사이징후 크기: " + file.size); // 크기가 6자리수
+
+         // blob 파일만 이제 전송하면 된다.
+         return thumbFile;
+     } else {
+         // size가 더 작으면 그냥 파일 전송
+         console.log("파일이 너무작아 resizing 안함");
+         return file;
+
+     }
+ }; // function imgToBlob end
+
+ // binary 파일 준비
+ function blobToBinary(imgFile, form_Data) {
+     var fileList = imgFile[0].files;
+
+
+     $.each(fileList, function(i, file) {
+         // 지속적 instance
+         var reader = new FileReader();
+         // 파일객체 해석
+         reader.readAsDataURL(file);
+         // reader 해석 완료 -> resizing img 생성 function 작동
+
+         reader.onload = function() {
+             // image객체 생성
+             var img = document.createElement("img");
+             // data-uri를 이미지 객체에 주입
+             img.src = reader.result;
+
+             // img 객체 주입 ->종류에 따라 blob 파일 생성하는 function 작동
+             img.onload = function() {
+
+                     // 상세 이미지9(S-DS-TYPE2) or 메인 섬네일(MS),상세
+                     // 섬네일(S-DS-TYPE1)에 따라 이름 붙여지는게 다름
+                     if (imgFile[0].id == "DetailSumnail") {
+
+                         console.log("상세 사진 저장")
+
+                         // 이미지 이름을 붙이고 FORM DATA에 저장
+                         form_Data.append("DetailSumnail" + registCount, imgToBlob(img, file, form_Data), "DetailSumnail" + registCount);
+                         registCount++;
+                         // FORM 데이터 들어 있는 객체 확인하기
+                         for (var key of form_Data.keys()) {
+
+                             console.log("form data key ------>" + key);
+
+                         }
+
+                         for (var value of form_Data.values()) {
+
+                             console.log("form data value ------>" + value);
+
+                         }
+
+                     } else if (imgFile[0].id == "MainSumnail") {
+                         console.log("메인 사진 저장")
+                             // 이미지 이름을 붙이고 FORM DATA에 저장
+                         form_Data.append("MainSumnail", imgToBlob(img, file, form_Data), "MainSumnail");
+                         // FORM 데이터 들어 있는 객체 확인하기
+                         for (var key of form_Data.keys()) {
+                             console.log("form data key ------>" + key);
+                         }
+                         for (var value of form_Data.values()) {
+                             console.log("form data value ------>" + value);
+                         }
+                     } else {
+                         console.log("상세 썸네일 사진  저장")
+                             // 이미지 이름을 붙이고 FORM DATA에 저장
+                         form_Data.append("DetailSumnail", imgToBlob(img, file, form_Data), "DetailSumnail");
+                         // FORM 데이터 들어 있는 객체 확인하기
+                         for (var key of form_Data.keys()) {
+                             console.log("form data key ------>" + key);
+                         }
+                         for (var value of form_Data.values()) {
+                             console.log("form data value ------>" + value);
+                         }
+                     } // else end
+
+                 } // img.onload=function() end
+
+         }; // reader.onload = function() end
+     }); // .each() end
+
+ } // blobToBynary() end
